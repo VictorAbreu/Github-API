dfe2f1a8b4f5d25941d261107cfff80ea7d3d75a
Victor Abreu <vlabreu@outlook.com>
2022-02-21 21:55:44 -0300

    Ajustes layout da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,16 +5,14 @@ import ButtonStart from 'components/ButtonStart';
 const Home = () => {
   return (
     <>
-      <div className="home-container">
-        <div className="home-card-principal">
-          <div className="home-content-container">
-            <h1>Desafio Github API</h1>
-            <p>Bootcamp Spring React - DevSuperior</p>
-            <div>
-              <NavLink to="/pesquisa">
-                <ButtonStart />
-              </NavLink>
-            </div>
+      <div className="home-card-principal">
+        <div className="home-content-container">
+          <h1>Desafio Github API</h1>
+          <p>Bootcamp Spring React - DevSuperior</p>
+          <div>
+            <NavLink to="/pesquisa">
+              <ButtonStart />
+            </NavLink>
           </div>
         </div>
       </div>
