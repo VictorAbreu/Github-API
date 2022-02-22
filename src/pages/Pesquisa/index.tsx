c00f737303e2dec18c37c6b5fd79384aba120dfc
Victor Abreu <vlabreu@outlook.com>
2022-02-21 22:10:25 -0300

    Ajustes css página Pesquisa

--- src/pages/Pesquisa/index.tsx
+++ src/pages/Pesquisa/index.tsx
@@ -6,8 +6,8 @@ import ButtonSearch from 'components/ButtonSearch';
 const Pesquisa = () => {
   return (
     <>
-      <div className="home-container">
-        <div className="home-card-principal">
+      <div className="search-container">
+        <div className="search-card-principal">
           <h1>Encontre um perfil do Github</h1>
           <input type="text" placeholder="Usuário Github" />
           <div>
